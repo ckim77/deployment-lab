6a46e5dbe7eda399eb9a063f98c70bf59a6acb0f
Chris Kim <ckjr92@gmail.com>
2021-11-18 13:25:59 -0700

    added rollbar

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
-const app = express()
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//rollbar
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'b0e54b6d8b3e4498b6f26f73063621d1',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, "../index.html"))
+    rollbar.info("HTML file served successfully");
 })
 
 app.use("/", express.static(path.join(__dirname, "../index.html")))
@@ -12,8 +27,10 @@ app.use("/", express.static(path.join(__dirname, "../index.html")))
 
 app.use("/css", express.static(path.join(__dirname, "../styles.css")))
 
+app.use(rollbar.errorHandler())
+
 const port = process.env.PORT || 4005
 
 app.listen(port, () => {
-    console.log(`jamming to port ${port}`)
+    console.log(`unlimited powerrrrrrrr on ${port}`)
 })
